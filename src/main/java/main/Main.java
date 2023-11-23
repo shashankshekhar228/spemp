bf33abb21f1ab86276e0c53b3ad6c0252034bd5c
shashank <sshekharsupermanreturnsagain@gmail.com>
2023-11-23 16:18:59 +0530

    test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+//hey
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args){
